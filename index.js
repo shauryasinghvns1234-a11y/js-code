4852c24a152b9e8466d3f5eb20f814fe6ebfb135
shaurya singh <shauryasinghvns1234@gmail.com>
2025-10-29 13:47:30 +0530

    hello

---
+++ index.js
@@ -0,0 +1,43 @@
+// var a=10;
+// //console.log(a)
+// var a =49;
+// //console.log(a)
+// var a=9;
+// //console.log(a)
+// a=89;
+// //console.log(a)
+// var a=19;
+// var b=29;
+// var c=a+b;
+// //console.log(c)
+// var x=17;
+// var z=15;
+// var y=x-z;
+// //console.log(y)
+// let a=67;
+// console.log(a)
+// let s=512;
+// s=343;
+// let m=565;
+// let i=s+m;
+// console.log(i) 
+// let w=23;
+// let b=14;
+// let u=w-b;
+// console.log(u)
+// let t=78;
+// let h=15;
+// let j=t*h;
+// console.log(j)
+// const f=73;
+// const g=67;
+// const l=f-g;
+// console.log(l)
+// const j=34;
+// const k=45;
+// const l=j*k;
+// console.log(l)
+const p=56;
+const u=32;
+const x=p/u;
+console.log(x)
